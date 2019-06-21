54e7a31995405ae31421ed47f9f52908363ec65c
Ilya <kras-05@mail.ru>
2019-06-21 16:08:01 +0300

    Дополнение.

--- PlacementNew.h
+++ PlacementNew.h
@@ -19,7 +19,7 @@ inline void D1()
 {
 	const unique_ptr<char[]> chr(new char[10]);
 	A* pa(new (chr.get()) A());
-	A* pb(new (chr.get() + sizeof(A)) A());
+	A* pb(new (chr.get() + sizeof(A)) A); // Можно писать и "A" вместо "A()".
 	pa->I = 0xAAAAAAAA;
 	pb->I = 0xBBBBBBBB;
 	// chr[10] = 1; // Можно.
@@ -39,7 +39,7 @@ inline void D2()
 	pa->~A();
 }
 
-inline void NewPlacingDemo()
+inline void PlacementNewDemo()
 {
 	D1();
 	D2();
