e11ee4833a01440179cfebd9ff4b2ef83fbe5429
Ilya <kras-05@mail.ru>
2020-01-21 01:42:37 +0300

    Отладка функций удаления нулей из массива и теста для них.

--- YandexSobesTest.h
+++ YandexSobesTest.h
@@ -87,6 +87,14 @@ inline bool VerifyResult(const vector<int>& v)
 	return true;
 }
 
+inline size_t OnesCount(const vector<int>& v)
+{
+	size_t r = 0;
+	for (const auto k : v)
+		r += k != 0;
+	return r;
+}
+
 class VectorGenerator
 {
 public:
@@ -100,20 +108,19 @@ public:
 
 		Iterator(VectorGenerator* cur) : _this(cur)
 		{
-			_this->Generate();
-			if (_this->_generated.empty())
+			if (_this->_signVector.empty())
 				_this = nullptr;
 		}
 
-		[[nodiscard]] vector<int>& operator*() const
+		[[nodiscard]] const vector<int>& operator*() const
 		{
-			return _this->_generated;
+			return _this->_signVector;
 		}
 
 		Iterator& operator++()
 		{
 			_this->Generate();
-			if (_this->_generated.empty())
+			if (_this->_signVector.empty())
 				_this = nullptr;
 			return *this;
 		}
@@ -124,72 +131,28 @@ public:
 		}
 	};
 
-	VectorGenerator(const size_t masLen, const int nonZeroValue) : _signVector(masLen), _masLen(masLen), _nonZeroValue(nonZeroValue)
-	{
-		if (nonZeroValue == 0)
-			throw;
-	}
+	VectorGenerator(const size_t masLen) : _signVector(masLen) { }
 
 private:
 	vector<int> _signVector;
-	size_t _masLen;
-	vector<int> _generated;
-	bool _firstGenerated = false;
-	const int _nonZeroValue;
 
 	void Generate()
 	{
-		if (_masLen == 0)
-		{
-			_generated = vector<int>();
-			return;
-		}
-
-		const auto incCounter = [this]
-		{
-			if (!_firstGenerated)
+		vector<int>::size_type k = _signVector.size();
+		while (k--)
+			if (int* t = &_signVector[k]; *t == 0)
 			{
-				_firstGenerated = true;
-				return true;
+				*t = 1;
+				int* lastElement = &_signVector[0] + _signVector.size();
+				if (int* t1 = t + 1; t1 < lastElement)
+					memset(t1, 0, (lastElement - t1) * sizeof(int));
+				return;
 			}
-			vector<int>::size_type k = _signVector.size();
-			while (k--)
-				if (int* t = &_signVector[k]; *t == 0)
-				{
-					*t = _nonZeroValue;
-					return true;
-				}
-				else
-				{
-					if (k == 0)
-						return false;
-					int* lastElement = &_signVector[0] + _signVector.size();
-					memset(t, 0, (lastElement - t) * sizeof(int));
-					++(*t);
-					return true;
-				}
-			return false;
-		};
-
-		if (!incCounter())
-		{
-			_generated = vector<int>();
-			return;
-		}
-
-		vector<int> t(_masLen);
-		vector<int>::size_type nonZeroCount = 0;
-		for (vector<int>::size_type k = 0; k < _signVector.size(); ++k)
-			if (_signVector[k] != 0)
-			{
-				t[k] = _nonZeroValue;
-				++nonZeroCount;
-			}
-
-		_generated = move(t);
+		_signVector.clear();
 	}
 
 public:
+	// ReSharper disable once CppInconsistentNaming
 	[[nodiscard]] Iterator begin()
 	{
 		return Iterator(this);
@@ -207,8 +170,6 @@ struct Result
 {
 	tuple<bool, long long, vector<int>> NonOpt;
 	tuple<bool, long long, vector<int>> Sobes;
-	tuple<bool, long long, vector<int>> SobesOrig;
-	tuple<bool, long long, vector<int>> Bubble;
 };
 
 inline Result RemoveZero(vector<int>& v)
@@ -229,35 +190,9 @@ inline Result RemoveZero(vector<int>& v)
 	//	}
 	//}
 
-	auto solSobesOrig = [v]() mutable -> tuple<bool, long long, vector<int>>
-	{
-		if (v.size() < 2)
-			return tuple<bool, long long, vector<int>>();
-		const steady_clock::time_point start = steady_clock::now();
-		for (vector<int>::size_type k = v.size() - 1; k >= 0; --k) // ОРИГИНАЛ
-		{
-			if (v[k] == 1)
-			{
-				v[k] = 0;
-				for (vector<int>::size_type i = 0; i < v.size(); ++i)  // NOLINT(modernize-loop-convert)
-				{
-					if (v[i] == 0)
-					{
-						v[i] = 1;
-						break;
-					}
-				}
-			}
-			if (k == 0)
-				break;
-		}
-		const steady_clock::time_point end = steady_clock::now();
-		return make_tuple(true, duration_cast<microseconds>(end - start).count(), v);
-	};
-
 	auto solNonOpt = [v]() mutable -> tuple<bool, long long, vector<int>>
 	{
-		if (v.size() < 2)
+		if (v.empty())
 			return tuple<bool, long long, vector<int>>();
 		const steady_clock::time_point start = steady_clock::now();
 		vector<int>::size_type k = 0;
@@ -265,92 +200,85 @@ inline Result RemoveZero(vector<int>& v)
 			if (*begin == 0)
 			{
 				v.erase(begin); // После этого итератор недействителен.
+				if (k != 0)
+					--k;
 				begin = v.begin() + k;
 				--k;
 			}
+			else
+				++begin;
 		const steady_clock::time_point end = steady_clock::now();
 		return make_tuple(true, duration_cast<microseconds>(end - start).count(), v);
 	};
 
 	auto solSobes = [v]() mutable -> tuple<bool, long long, vector<int>>
 	{
-		if (v.size() < 2)
+		if (v.empty())
 			return tuple<bool, long long, vector<int>>();
 		const steady_clock::time_point start = steady_clock::now();
-		vector<int>::size_type lastZero = -1;
+		vector<int>::size_type lastZero = 0;
+		bool needResize = false;
 		vector<int>::size_type k = v.size() - 1;
 		do
 		{
+			if (needResize && k == lastZero)
+			{
+				++lastZero;
+				needResize = true;
+				break;
+			}
 			if (v[k] == 0)
-				continue;
-			v[k] = 0;
-			for (vector<int>::size_type i = lastZero + 1; i < v.size(); ++i)
-				if (v[i] == 0)
-				{
-					v[i] = 1;
-					lastZero = i;
-					break;
-				}
-			if (k == 0 || k <= (lastZero + 2))
+			{
+				if (k != 0)
+					continue;
+				needResize = true;
 				break;
-		} while (--k);
-		v.resize(lastZero + 1);
-		const steady_clock::time_point end = steady_clock::now();
-		return make_tuple(true, duration_cast<microseconds>(end - start).count(), v);
-	};
-
-	auto solBubble = [v]() mutable -> tuple<bool, long long, vector<int>>
-	{
-		if (v.size() < 2)
-			return tuple<bool, long long, vector<int>>();
-
-		auto getValue = [&v](const vector<int>::size_type k) -> optional<int>
-		{
-			for (vector<int>::size_type j = k + 1; j < v.size(); ++j)
-				if (int& t = v[j]; t != 0)
-				{
-					const int r = t;
-					t = 0;
-					return r;
-				}
-			return nullopt;
-		};
-
-		const steady_clock::time_point start = steady_clock::now();
-		for (vector<int>::size_type k = 0; k < v.size(); ++k)
-		{
-			if (v[k] != 0)
-				continue;
-			if (optional<int> value = getValue(k))
-				v[k] = *value;
-			else
+			}
+			vector<int>::size_type i = lastZero;
+			for (; i < k; ++i)
+			{
+				if (v[i] != 0)
+					continue;
+				v[i] = 1;
+				v[k] = 0;
+				lastZero = i + 1;
+				needResize = true;
 				break;
-		}
+			}
+			if (i == k)
+			{
+				lastZero = i + 1;
+				needResize = true;
+				break;
+			}
+		} while (k--);
+		if (needResize)
+			v.resize(lastZero);
 		const steady_clock::time_point end = steady_clock::now();
 		return make_tuple(true, duration_cast<microseconds>(end - start).count(), v);
 	};
 
 	auto nonOpt = solNonOpt();
 	auto sobes = solSobes();
-	auto sobesOrig = solSobesOrig();
-	auto bubble = solBubble();
 
-	const bool b = get<0>(nonOpt);
-	if (b != get<0>(sobes) || b != get<0>(sobesOrig) || b != get<0>(bubble))
+	if (get<0>(nonOpt) != get<0>(sobes))
 		throw;
 
-	if (!VerifyResult(get<2>(nonOpt)) || !VerifyResult(get<2>(sobes)) || !VerifyResult(get<2>(sobesOrig)) || !VerifyResult(get<2>(bubble)))
+	if (!VerifyResult(get<2>(nonOpt)) || !VerifyResult(get<2>(sobes)))
 		throw;
 
-	if (!VectorCompare(get<2>(nonOpt), get<2>(sobes)) || !VectorCompare(get<2>(nonOpt), get<2>(sobesOrig)) || !VectorCompare(get<2>(nonOpt), get<2>(bubble)))
+	if (!VectorCompare(get<2>(nonOpt), get<2>(sobes)))
 		throw;
 
-	return Result{ move(nonOpt),move(sobes), move(sobesOrig), move(bubble) };
+	if (OnesCount(v) != OnesCount(get<2>(sobes)))
+		throw;
+
+	return Result{ move(nonOpt),move(sobes) };
 }
 
-inline void RemoveTest(const size_t masLen, const int nonZeroValue)
+inline void RemoveTest(const size_t masLen)
 {
-	if (masLen == 0 || nonZeroValue == 0)
+	if (masLen == 0)
 		throw;
 
 	const auto getVectorSize = [masLen]
@@ -363,13 +291,16 @@ inline void RemoveTest(const size_t masLen, const int nonZeroValue)
 
 	vector<Result> d;
 	d.reserve(getVectorSize());
-	for (vector<int>& v : VectorGenerator(masLen, nonZeroValue))
-		d.emplace_back(RemoveZero(v));
+	for (const vector<int>& v : VectorGenerator(masLen))
+	{
+		vector<int> t = v;
+		d.emplace_back(RemoveZero(t));
+	}
 
-	long long maxTimeNonOpt = 0, maxTimeSobes = 0, maxTimeSobesOrig = 0, maxTimeBubble = 0;
-	long long maxNonOpt = 0, maxSobes = 0, maxSobesOrig = 0, maxBubble = 0;
-	long long timeNonOptMax = 0, timeSobesMax = 0, timeSobesOrigMax = 0, timeBubbleMax = 0;
-	long long timeNonOptMin = LLONG_MAX, timeSobesMin = LLONG_MAX, timeSobesOrigMin = LLONG_MAX, timeBubbleMin = LLONG_MAX;
+	long long maxTimeNonOpt = 0, maxTimeSobes = 0;
+	long long maxNonOpt = 0, maxSobes = 0;
+	long long timeNonOptMax = 0, timeSobesMax = 0;
+	long long timeNonOptMin = LLONG_MAX, timeSobesMin = LLONG_MAX;
 	for (const auto& v : d)
 	{
 		if (get<0>(v.NonOpt))
@@ -392,26 +323,6 @@ inline void RemoveTest(const size_t masLen, const int nonZeroValue)
 				timeSobesMin = t;
 			++maxSobes;
 		}
-		if (get<0>(v.SobesOrig))
-		{
-			const long long t = get<1>(v.SobesOrig);
-			maxTimeSobesOrig += t;
-			if (timeSobesOrigMax < t)
-				timeSobesOrigMax = t;
-			if (timeSobesOrigMin > t)
-				timeSobesOrigMin = t;
-			++maxSobesOrig;
-		}
-		if (get<0>(v.Bubble))
-		{
-			const long long t = get<1>(v.Bubble);
-			maxTimeBubble += t;
-			if (timeBubbleMax < t)
-				timeBubbleMax = t;
-			if (timeBubbleMin > t)
-				timeBubbleMin = t;
-			++maxBubble;
-		}
 	}
 
 	if (maxNonOpt != 0)
@@ -420,12 +331,6 @@ inline void RemoveTest(const size_t masLen, const int nonZeroValue)
 	if (maxSobes != 0)
 		// ReSharper disable once CppAssignedValueIsNeverUsed
 		maxTimeSobes /= maxSobes;
-	if (maxSobesOrig != 0)
-		// ReSharper disable once CppAssignedValueIsNeverUsed
-		maxTimeSobesOrig /= maxSobesOrig;
-	if (maxBubble != 0)
-		// ReSharper disable once CppAssignedValueIsNeverUsed
-		maxTimeBubble /= maxBubble;
 }
 
 inline void YandexTest()
@@ -435,9 +340,9 @@ inline void YandexTest()
 		B* pb = new B();
 		const Foo f(pa, pb);
 		// ReSharper disable once CppDeclaratorNeverUsed
-		const Foo d(f);
+		const Foo d(f);  // NOLINT(performance-unnecessary-copy-initialization)
 		//_asm int 3;
 	}
 
-	RemoveTest(1, 1);
+	RemoveTest(2);
 }
