bdf733af750c25979db233b6c56ea296f49726f3
Ilya <kras-05@mail.ru>
2020-03-17 15:10:59 +0300

    Временно.

--- SEHExamplesStart.cpp
+++ SEHExamplesStart.cpp
@@ -60,11 +60,73 @@
 #include "KasperskyTest.h"
 #include "OperatorOverload.h"
 #include "FINTECHTest.h"*/
+#include <Windows.h>
+#include <memory>
 
 int __cdecl main()
 {
 	try
 	{
+		LPTSTR pszBuf = (LPTSTR)LocalAlloc(LPTR, MAX_PATH * sizeof(TCHAR));
+		//_asm int 3;
+		PSECURITY_DESCRIPTOR pSD = (PSECURITY_DESCRIPTOR)GlobalAlloc(GMEM_MOVEABLE, sizeof(PSECURITY_DESCRIPTOR));
+		//_asm int 3;
+
+		MoveMemory(pszBuf, pSD, 1);
+
+		//_asm int 3;
+
+		std::unique_ptr<int> t[] = { std::unique_ptr<int>(new int(9)), std::unique_ptr<int>(new int(8)) };
+		for (auto&& f : t) // от ссылки обычной или на R_Value ничего не меняется
+		{
+			int* gh = f.get();
+			std::unique_ptr<int> h = move(f);
+			_asm nop;
+		}
+
+		class A
+		{
+			int t;
+		};
+
+		class B : A
+		{
+
+		};
+
+		const unsigned sd = sizeof(B);
+		//const double sd1 = 5.0 / 0; // бесконечность
+
+		HANDLE h = HeapCreate(0, 0, 8192);
+		LPVOID v = HeapAlloc(h, HEAP_ZERO_MEMORY, 1024);
+
+		unsigned char* mem = reinterpret_cast<unsigned char*>(v);
+
+		for (unsigned r = 0; r < 1024; ++r)
+			*mem++ = 0;
+
+		LPVOID v1 = HeapAlloc(h, HEAP_ZERO_MEMORY, 2048); // если этого вызова не будет, последующий цикл будет слетать
+
+		unsigned char* mem1 = reinterpret_cast<unsigned char*>(v);
+
+		for (unsigned r = 0; r < 3000; ++r)
+			*mem1++ = 0;
+
+		unsigned char* p = (unsigned char*)VirtualAlloc(NULL, 8192, MEM_RESERVE, PAGE_READWRITE);
+		p = (unsigned char*)VirtualAlloc(p, 4096, MEM_COMMIT, PAGE_READWRITE);
+
+		for (unsigned r = 0; r < 4096; ++r) // не более 4096
+			*p++ = 0;
+
+		p = (unsigned char*)VirtualAlloc(p, 8192, MEM_COMMIT, PAGE_READWRITE);
+
+		for (unsigned r = 0; r < 8192; ++r)
+			*p++ = 0;
+
+		int ui = 9;
+		unsigned er = ui;
+		ui = er;
+		
 		return 0;
 	}
 	catch (...)
